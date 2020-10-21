725b6baadc331c785835fec9780455d6156f2043
Sophia Robert <sophia.r.robert@gmail.com>
2020-10-21 13:10:06 -0400

    added pracInstruct and prac_instr_run1

--- global_local-legacy-browsers.js
+++ global_local-legacy-browsers.js
@@ -17,7 +17,7 @@ psychoJS.openWindow({
 
 // store info about the experiment session:
 let expName = 'global_local';  // from the Builder filename that created this script
-let expInfo = {'participant': '', 'session': '001'};
+let expInfo = {'participant': 'SR', 'design': '1', 'position': '0'};
 
 // schedule the experiment:
 psychoJS.schedule(psychoJS.gui.DlgFromDict({
@@ -35,6 +35,12 @@ flowScheduler.add(experimentInit);
 flowScheduler.add(screen_scaleRoutineBegin());
 flowScheduler.add(screen_scaleRoutineEachFrame());
 flowScheduler.add(screen_scaleRoutineEnd());
+flowScheduler.add(pracInstructRoutineBegin());
+flowScheduler.add(pracInstructRoutineEachFrame());
+flowScheduler.add(pracInstructRoutineEnd());
+flowScheduler.add(prac_instr_run1RoutineBegin());
+flowScheduler.add(prac_instr_run1RoutineEachFrame());
+flowScheduler.add(prac_instr_run1RoutineEnd());
 flowScheduler.add(quitPsychoJS, '', true);
 
 // quit if user presses Cancel in dialog box:
@@ -95,6 +101,21 @@ var width;
 var text_top;
 var text_bottom;
 var ccimage;
+var pracInstructClock;
+var fixColor_opts;
+var prac1_fixColSwitch;
+var prac2_fixColSwitch;
+var fixColorIdx_Run1;
+var fixColorIdx_Run2;
+var start_exp_text;
+var start_exp_press;
+var prac_instr_run1Clock;
+var instructions_run1;
+var cond_file_run1;
+var rand8Idx;
+var rand4rows_run1;
+var instructions_imageR1;
+var key_resp;
 var globalClock;
 var routineTimer;
 function experimentInit() {
@@ -201,6 +222,72 @@ function experimentInit() {
     flipHoriz : false, flipVert : false,
     texRes : 512, interpolate : true, depth : -4.0 
   });
+  // Initialize components for Routine "pracInstruct"
+  pracInstructClock = new util.Clock();
+  fixColor_opts = ["orange", "pink"];
+  shuffle(fixColor_opts);
+  prac1_fixColSwitch = [0, 0, 0, 1];
+  shuffle(prac1_fixColSwitch);
+  prac2_fixColSwitch = [0, 0, 0, 1];
+  shuffle(prac2_fixColSwitch);
+  fixColorIdx_Run1 = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1];
+  shuffle(fixColorIdx_Run1);
+  fixColorIdx_Run2 = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1];
+  shuffle(fixColorIdx_Run2);
+  thisExp.addData("fixpR1", prac1_fixColSwitch);
+  thisExp.addData("fixR1", fixColorIdx_Run1);
+  thisExp.addData("fixpR2", prac2_fixColSwitch);
+  thisExp.addData("fixR2", fixColorIdx_Run2);
+  
+  start_exp_text = new visual.TextStim({
+    win: psychoJS.window,
+    name: 'start_exp_text',
+    text: 'You will play two games. \nBefore you start, let’s do some practice!\n\nAre you ready? \nPress Space to start practice!',
+    font: 'Arial',
+    units: undefined, 
+    pos: [0, 0], height: 0.05,  wrapWidth: undefined, ori: 0,
+    color: new util.Color('white'),  opacity: 1,
+    depth: -1.0 
+  });
+  
+  start_exp_press = new core.Keyboard({psychoJS: psychoJS, clock: new util.Clock(), waitForStart: true});
+  
+  // Initialize components for Routine "prac_instr_run1"
+  prac_instr_run1Clock = new util.Clock();
+  if ((expInfo["design"] === "1")) {
+      instructions_run1 = "Designs/design1_run1.png";
+      cond_file_run1 = "Designs/design1_run1.csv";
+  } else {
+      if ((expInfo["design"] === "2")) {
+          instructions_run1 = "Designs/design2_run1.png";
+          cond_file_run1 = "Designs/design2_run1.csv";
+      } else {
+          if ((expInfo["design"] === "3")) {
+              instruction_run1 = "Designs/design3_run1.png";
+              cond_file_run1 = "Designs/design3_run1.csv";
+          } else {
+              if ((expInfo["design"] === "4")) {
+                  instructions_run1 = "Designs/design4_run1.png";
+                  cond_file_run1 = "Designs/design4_run1.csv";
+              }
+          }
+      }
+  }
+  rand8Idx = [0, 1, 2, 3, 4, 5, 6, 7];
+  shuffle(rand8Idx);
+  rand4rows_run1 = rand8Idx.slice(0, 4);
+  
+  instructions_imageR1 = new visual.ImageStim({
+    win : psychoJS.window,
+    name : 'instructions_imageR1', units : undefined, 
+    image : undefined, mask : undefined,
+    ori : 0, pos : [0, 0], size : [(0.75 * 1.5), 0.75],
+    color : new util.Color([1, 1, 1]), opacity : 1,
+    flipHoriz : false, flipVert : false,
+    texRes : 512, interpolate : true, depth : -1.0 
+  });
+  key_resp = new core.Keyboard({psychoJS: psychoJS, clock: new util.Clock(), waitForStart: true});
+  
   // Create some handy timers
   globalClock = new util.Clock();  // to track the time since experiment started
   routineTimer = new util.CountdownTimer();  // to track time remaining of each (non-slip) routine
@@ -373,6 +460,231 @@ function screen_scaleRoutineEnd(snapshot) {
 }
 
 
+var _start_exp_press_allKeys;
+var pracInstructComponents;
+function pracInstructRoutineBegin(snapshot) {
+  return function () {
+    //------Prepare to start Routine 'pracInstruct'-------
+    t = 0;
+    pracInstructClock.reset(); // clock
+    frameN = -1;
+    // update component parameters for each repeat
+    start_exp_press.keys = undefined;
+    start_exp_press.rt = undefined;
+    _start_exp_press_allKeys = [];
+    // keep track of which components have finished
+    pracInstructComponents = [];
+    pracInstructComponents.push(start_exp_text);
+    pracInstructComponents.push(start_exp_press);
+    
+    pracInstructComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent)
+        thisComponent.status = PsychoJS.Status.NOT_STARTED;
+       });
+    
+    return Scheduler.Event.NEXT;
+  };
+}
+
+
+function pracInstructRoutineEachFrame(snapshot) {
+  return function () {
+    //------Loop for each frame of Routine 'pracInstruct'-------
+    let continueRoutine = true; // until we're told otherwise
+    // get current time
+    t = pracInstructClock.getTime();
+    frameN = frameN + 1;// number of completed frames (so 0 is the first frame)
+    // update/draw components on each frame
+    
+    // *start_exp_text* updates
+    if (t >= 0.0 && start_exp_text.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      start_exp_text.tStart = t;  // (not accounting for frame time here)
+      start_exp_text.frameNStart = frameN;  // exact frame index
+      
+      start_exp_text.setAutoDraw(true);
+    }
+
+    
+    // *start_exp_press* updates
+    if (t >= 1 && start_exp_press.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      start_exp_press.tStart = t;  // (not accounting for frame time here)
+      start_exp_press.frameNStart = frameN;  // exact frame index
+      
+      // keyboard checking is just starting
+      psychoJS.window.callOnFlip(function() { start_exp_press.clock.reset(); });  // t=0 on next screen flip
+      psychoJS.window.callOnFlip(function() { start_exp_press.start(); }); // start on screen flip
+      psychoJS.window.callOnFlip(function() { start_exp_press.clearEvents(); });
+    }
+
+    if (start_exp_press.status === PsychoJS.Status.STARTED) {
+      let theseKeys = start_exp_press.getKeys({keyList: ['space'], waitRelease: false});
+      _start_exp_press_allKeys = _start_exp_press_allKeys.concat(theseKeys);
+      if (_start_exp_press_allKeys.length > 0) {
+        start_exp_press.keys = _start_exp_press_allKeys[_start_exp_press_allKeys.length - 1].name;  // just the last key pressed
+        start_exp_press.rt = _start_exp_press_allKeys[_start_exp_press_allKeys.length - 1].rt;
+        // a response ends the routine
+        continueRoutine = false;
+      }
+    }
+    
+    // check for quit (typically the Esc key)
+    if (psychoJS.experiment.experimentEnded || psychoJS.eventManager.getKeys({keyList:['escape']}).length > 0) {
+      return quitPsychoJS('The [Escape] key was pressed. Goodbye!', false);
+    }
+    
+    // check if the Routine should terminate
+    if (!continueRoutine) {  // a component has requested a forced-end of Routine
+      return Scheduler.Event.NEXT;
+    }
+    
+    continueRoutine = false;  // reverts to True if at least one component still running
+    pracInstructComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent && thisComponent.status !== PsychoJS.Status.FINISHED) {
+        continueRoutine = true;
+      }
+    });
+    
+    // refresh the screen if continuing
+    if (continueRoutine) {
+      return Scheduler.Event.FLIP_REPEAT;
+    } else {
+      return Scheduler.Event.NEXT;
+    }
+  };
+}
+
+
+function pracInstructRoutineEnd(snapshot) {
+  return function () {
+    //------Ending Routine 'pracInstruct'-------
+    pracInstructComponents.forEach( function(thisComponent) {
+      if (typeof thisComponent.setAutoDraw === 'function') {
+        thisComponent.setAutoDraw(false);
+      }
+    });
+    // the Routine "pracInstruct" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset();
+    
+    return Scheduler.Event.NEXT;
+  };
+}
+
+
+var _key_resp_allKeys;
+var prac_instr_run1Components;
+function prac_instr_run1RoutineBegin(snapshot) {
+  return function () {
+    //------Prepare to start Routine 'prac_instr_run1'-------
+    t = 0;
+    prac_instr_run1Clock.reset(); // clock
+    frameN = -1;
+    // update component parameters for each repeat
+    instructions_imageR1.setImage(instructions_run1);
+    key_resp.keys = undefined;
+    key_resp.rt = undefined;
+    _key_resp_allKeys = [];
+    // keep track of which components have finished
+    prac_instr_run1Components = [];
+    prac_instr_run1Components.push(instructions_imageR1);
+    prac_instr_run1Components.push(key_resp);
+    
+    prac_instr_run1Components.forEach( function(thisComponent) {
+      if ('status' in thisComponent)
+        thisComponent.status = PsychoJS.Status.NOT_STARTED;
+       });
+    
+    return Scheduler.Event.NEXT;
+  };
+}
+
+
+function prac_instr_run1RoutineEachFrame(snapshot) {
+  return function () {
+    //------Loop for each frame of Routine 'prac_instr_run1'-------
+    let continueRoutine = true; // until we're told otherwise
+    // get current time
+    t = prac_instr_run1Clock.getTime();
+    frameN = frameN + 1;// number of completed frames (so 0 is the first frame)
+    // update/draw components on each frame
+    
+    // *instructions_imageR1* updates
+    if (t >= 0.0 && instructions_imageR1.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      instructions_imageR1.tStart = t;  // (not accounting for frame time here)
+      instructions_imageR1.frameNStart = frameN;  // exact frame index
+      
+      instructions_imageR1.setAutoDraw(true);
+    }
+
+    
+    // *key_resp* updates
+    if (t >= 1 && key_resp.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      key_resp.tStart = t;  // (not accounting for frame time here)
+      key_resp.frameNStart = frameN;  // exact frame index
+      
+      // keyboard checking is just starting
+      psychoJS.window.callOnFlip(function() { key_resp.clock.reset(); });  // t=0 on next screen flip
+      psychoJS.window.callOnFlip(function() { key_resp.start(); }); // start on screen flip
+      psychoJS.window.callOnFlip(function() { key_resp.clearEvents(); });
+    }
+
+    if (key_resp.status === PsychoJS.Status.STARTED) {
+      let theseKeys = key_resp.getKeys({keyList: ['space'], waitRelease: false});
+      _key_resp_allKeys = _key_resp_allKeys.concat(theseKeys);
+      if (_key_resp_allKeys.length > 0) {
+        key_resp.keys = _key_resp_allKeys[_key_resp_allKeys.length - 1].name;  // just the last key pressed
+        key_resp.rt = _key_resp_allKeys[_key_resp_allKeys.length - 1].rt;
+        // a response ends the routine
+        continueRoutine = false;
+      }
+    }
+    
+    // check for quit (typically the Esc key)
+    if (psychoJS.experiment.experimentEnded || psychoJS.eventManager.getKeys({keyList:['escape']}).length > 0) {
+      return quitPsychoJS('The [Escape] key was pressed. Goodbye!', false);
+    }
+    
+    // check if the Routine should terminate
+    if (!continueRoutine) {  // a component has requested a forced-end of Routine
+      return Scheduler.Event.NEXT;
+    }
+    
+    continueRoutine = false;  // reverts to True if at least one component still running
+    prac_instr_run1Components.forEach( function(thisComponent) {
+      if ('status' in thisComponent && thisComponent.status !== PsychoJS.Status.FINISHED) {
+        continueRoutine = true;
+      }
+    });
+    
+    // refresh the screen if continuing
+    if (continueRoutine) {
+      return Scheduler.Event.FLIP_REPEAT;
+    } else {
+      return Scheduler.Event.NEXT;
+    }
+  };
+}
+
+
+function prac_instr_run1RoutineEnd(snapshot) {
+  return function () {
+    //------Ending Routine 'prac_instr_run1'-------
+    prac_instr_run1Components.forEach( function(thisComponent) {
+      if (typeof thisComponent.setAutoDraw === 'function') {
+        thisComponent.setAutoDraw(false);
+      }
+    });
+    // the Routine "prac_instr_run1" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset();
+    
+    return Scheduler.Event.NEXT;
+  };
+}
+
+
 function endLoopIteration(scheduler, snapshot) {
   // ------Prepare for next entry------
   return function () {
@@ -409,6 +721,10 @@ function quitPsychoJS(message, isCompleted) {
   if (psychoJS.experiment.isEntryEmpty()) {
     psychoJS.experiment.nextEntry();
   }
+  
+  
+  
+  
   
   
   
--- global_local_lastrun.py
+++ global_local_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.4),
-    on Wed Oct 21 12:47:24 2020
+    on Wed Oct 21 13:07:59 2020
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -37,7 +37,7 @@ os.chdir(_thisDir)
 # Store info about the experiment session
 psychopyVersion = '2020.2.4'
 expName = 'global_local'  # from the Builder filename that created this script
-expInfo = {'participant': '', 'session': '001'}
+expInfo = {'participant': '', 'design': '1', 'position': '0'}
 dlg = gui.DlgFromDict(dictionary=expInfo, sort_keys=False, title=expName)
 if dlg.OK == False:
     core.quit()  # user pressed cancel
@@ -131,6 +131,84 @@ ccimage = visual.ImageStim(
     color=[1,1,1], colorSpace='rgb', opacity=1,
     flipHoriz=False, flipVert=False,
     texRes=512, interpolate=True, depth=-4.0)
+
+# Initialize components for Routine "pracInstruct"
+pracInstructClock = core.Clock()
+
+fixColor_opts = ["orange", "pink"];
+shuffle(fixColor_opts);
+prac1_fixColSwitch = [0,0,0,1];
+shuffle(prac1_fixColSwitch);
+prac2_fixColSwitch = [0,0,0,1];
+shuffle(prac2_fixColSwitch);
+fixColorIdx_Run1 =  [0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1,
+                                   0, 0, 0, 0, 0, 0, 0, 1];
+shuffle(fixColorIdx_Run1);
+fixColorIdx_Run2 = [0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1, 
+                                   0, 0, 0, 0, 0, 0, 0, 1,
+                                   0, 0, 0, 0, 0, 0, 0, 1];
+shuffle(fixColorIdx_Run2);
+
+
+thisExp.addData('fixpR1', prac1_fixColSwitch)
+thisExp.addData('fixR1', fixColorIdx_Run1)
+thisExp.addData('fixpR2', prac2_fixColSwitch)
+thisExp.addData('fixR2', fixColorIdx_Run2)
+start_exp_text = visual.TextStim(win=win, name='start_exp_text',
+    text='You will play two games. \nBefore you start, let’s do some practice!\n\nAre you ready? \nPress ’Space’ to start practice!',
+    font='Arial',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1, 
+    languageStyle='LTR',
+    depth=-1.0);
+start_exp_press = keyboard.Keyboard()
+
+# Initialize components for Routine "prac_instr_run1"
+prac_instr_run1Clock = core.Clock()
+if expInfo['design']=='1':
+    instructions_run1 = 'Designs/design1_run1.png'
+    cond_file_run1 = 'Designs/design1_run1.csv'
+elif expInfo['design']=='2':
+    instructions_run1 = 'Designs/design2_run1.png'
+    cond_file_run1 = 'Designs/design2_run1.csv'
+elif expInfo['design']=='3':
+    instruction_run1 = 'Designs/design3_run1.png'
+    cond_file_run1 = 'Designs/design3_run1.csv'
+elif expInfo['design']=='4':
+    instructions_run1 = 'Designs/design4_run1.png'
+    cond_file_run1 = 'Designs/design4_run1.csv'
+
+rand8Idx = [0,1,2,3,4,5,6,7];
+shuffle(rand8Idx)
+rand4rows_run1 = rand8Idx[0:4]
+instructions_imageR1 = visual.ImageStim(
+    win=win,
+    name='instructions_imageR1', 
+    image='sin', mask=None,
+    ori=0, pos=(0, 0), size=(0.75*1.5, 0.75),
+    color=[1,1,1], colorSpace='rgb', opacity=1,
+    flipHoriz=False, flipVert=False,
+    texRes=512, interpolate=True, depth=-1.0)
+key_resp = keyboard.Keyboard()
 
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
@@ -243,6 +321,181 @@ thisExp.addData('text_bottom.stopped', text_bottom.tStopRefresh)
 thisExp.addData('ccimage.started', ccimage.tStartRefresh)
 thisExp.addData('ccimage.stopped', ccimage.tStopRefresh)
 # the Routine "screen_scale" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+
+# ------Prepare to start Routine "pracInstruct"-------
+continueRoutine = True
+# update component parameters for each repeat
+start_exp_press.keys = []
+start_exp_press.rt = []
+_start_exp_press_allKeys = []
+# keep track of which components have finished
+pracInstructComponents = [start_exp_text, start_exp_press]
+for thisComponent in pracInstructComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+pracInstructClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+
+# -------Run Routine "pracInstruct"-------
+while continueRoutine:
+    # get current time
+    t = pracInstructClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=pracInstructClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *start_exp_text* updates
+    if start_exp_text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        start_exp_text.frameNStart = frameN  # exact frame index
+        start_exp_text.tStart = t  # local t and not account for scr refresh
+        start_exp_text.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(start_exp_text, 'tStartRefresh')  # time at next scr refresh
+        start_exp_text.setAutoDraw(True)
+    
+    # *start_exp_press* updates
+    waitOnFlip = False
+    if start_exp_press.status == NOT_STARTED and tThisFlip >= 1-frameTolerance:
+        # keep track of start time/frame for later
+        start_exp_press.frameNStart = frameN  # exact frame index
+        start_exp_press.tStart = t  # local t and not account for scr refresh
+        start_exp_press.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(start_exp_press, 'tStartRefresh')  # time at next scr refresh
+        start_exp_press.status = STARTED
+        # keyboard checking is just starting
+        waitOnFlip = True
+        win.callOnFlip(start_exp_press.clock.reset)  # t=0 on next screen flip
+        win.callOnFlip(start_exp_press.clearEvents, eventType='keyboard')  # clear events on next screen flip
+    if start_exp_press.status == STARTED and not waitOnFlip:
+        theseKeys = start_exp_press.getKeys(keyList=['space'], waitRelease=False)
+        _start_exp_press_allKeys.extend(theseKeys)
+        if len(_start_exp_press_allKeys):
+            start_exp_press.keys = _start_exp_press_allKeys[-1].name  # just the last key pressed
+            start_exp_press.rt = _start_exp_press_allKeys[-1].rt
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in pracInstructComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "pracInstruct"-------
+for thisComponent in pracInstructComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+thisExp.addData('start_exp_text.started', start_exp_text.tStartRefresh)
+thisExp.addData('start_exp_text.stopped', start_exp_text.tStopRefresh)
+# the Routine "pracInstruct" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+
+# ------Prepare to start Routine "prac_instr_run1"-------
+continueRoutine = True
+# update component parameters for each repeat
+instructions_imageR1.setImage(instructions_run1)
+key_resp.keys = []
+key_resp.rt = []
+_key_resp_allKeys = []
+# keep track of which components have finished
+prac_instr_run1Components = [instructions_imageR1, key_resp]
+for thisComponent in prac_instr_run1Components:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+prac_instr_run1Clock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+
+# -------Run Routine "prac_instr_run1"-------
+while continueRoutine:
+    # get current time
+    t = prac_instr_run1Clock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=prac_instr_run1Clock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *instructions_imageR1* updates
+    if instructions_imageR1.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        instructions_imageR1.frameNStart = frameN  # exact frame index
+        instructions_imageR1.tStart = t  # local t and not account for scr refresh
+        instructions_imageR1.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(instructions_imageR1, 'tStartRefresh')  # time at next scr refresh
+        instructions_imageR1.setAutoDraw(True)
+    
+    # *key_resp* updates
+    waitOnFlip = False
+    if key_resp.status == NOT_STARTED and tThisFlip >= 1-frameTolerance:
+        # keep track of start time/frame for later
+        key_resp.frameNStart = frameN  # exact frame index
+        key_resp.tStart = t  # local t and not account for scr refresh
+        key_resp.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(key_resp, 'tStartRefresh')  # time at next scr refresh
+        key_resp.status = STARTED
+        # keyboard checking is just starting
+        waitOnFlip = True
+        win.callOnFlip(key_resp.clock.reset)  # t=0 on next screen flip
+        win.callOnFlip(key_resp.clearEvents, eventType='keyboard')  # clear events on next screen flip
+    if key_resp.status == STARTED and not waitOnFlip:
+        theseKeys = key_resp.getKeys(keyList=['space'], waitRelease=False)
+        _key_resp_allKeys.extend(theseKeys)
+        if len(_key_resp_allKeys):
+            key_resp.keys = _key_resp_allKeys[-1].name  # just the last key pressed
+            key_resp.rt = _key_resp_allKeys[-1].rt
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in prac_instr_run1Components:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "prac_instr_run1"-------
+for thisComponent in prac_instr_run1Components:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+thisExp.addData('instructions_imageR1.started', instructions_imageR1.tStartRefresh)
+thisExp.addData('instructions_imageR1.stopped', instructions_imageR1.tStopRefresh)
+# the Routine "prac_instr_run1" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
 # Flip one final time so any remaining win.callOnFlip() 
