7e28dd9464fdfdd28918f0a057228e29c57cfd89
Sophia Robert <sophia.r.robert@gmail.com>
2020-10-21 11:56:14 -0400

    card_routine

---
+++ global_local-legacy-browsers.js
@@ -0,0 +1,420 @@
+﻿/********************* 
+ * Global_Local Test *
+ *********************/
+
+// init psychoJS:
+const psychoJS = new PsychoJS({
+  debug: true
+});
+
+// open window:
+psychoJS.openWindow({
+  fullscr: true,
+  color: new util.Color([0, 0, 0]),
+  units: 'height',
+  waitBlanking: true
+});
+
+// store info about the experiment session:
+let expName = 'global_local';  // from the Builder filename that created this script
+let expInfo = {'participant': '', 'session': '001'};
+
+// schedule the experiment:
+psychoJS.schedule(psychoJS.gui.DlgFromDict({
+  dictionary: expInfo,
+  title: expName
+}));
+
+const flowScheduler = new Scheduler(psychoJS);
+const dialogCancelScheduler = new Scheduler(psychoJS);
+psychoJS.scheduleCondition(function() { return (psychoJS.gui.dialogComponent.button === 'OK'); }, flowScheduler, dialogCancelScheduler);
+
+// flowScheduler gets run if the participants presses OK
+flowScheduler.add(updateInfo); // add timeStamp
+flowScheduler.add(experimentInit);
+flowScheduler.add(screen_scaleRoutineBegin());
+flowScheduler.add(screen_scaleRoutineEachFrame());
+flowScheduler.add(screen_scaleRoutineEnd());
+flowScheduler.add(quitPsychoJS, '', true);
+
+// quit if user presses Cancel in dialog box:
+dialogCancelScheduler.add(quitPsychoJS, '', false);
+
+psychoJS.start({
+  expName: expName,
+  expInfo: expInfo,
+  resources: [
+  ]
+});
+
+psychoJS.experimentLogger.setLevel(core.Logger.ServerLevel.EXP);
+
+
+var frameDur;
+function updateInfo() {
+  expInfo['date'] = util.MonotonicClock.getDateStr();  // add a simple timestamp
+  expInfo['expName'] = expName;
+  expInfo['psychopyVersion'] = '2020.2.4';
+  expInfo['OS'] = window.navigator.platform;
+
+  // store frame rate of monitor if we can measure it successfully
+  expInfo['frameRate'] = psychoJS.window.getActualFrameRate();
+  if (typeof expInfo['frameRate'] !== 'undefined')
+    frameDur = 1.0 / Math.round(expInfo['frameRate']);
+  else
+    frameDur = 1.0 / 60.0; // couldn't get a reliable measure so guess
+
+  // add info from the URL:
+  util.addInfoFromUrl(expInfo);
+  
+  return Scheduler.Event.NEXT;
+}
+
+
+var screen_scaleClock;
+var thisExp;
+var win;
+var event;
+var shuffle;
+var webbrowser;
+var random;
+var randint;
+var round;
+var oldt;
+var x_size;
+var y_size;
+var screen_height;
+var x_scale;
+var y_scale;
+var dbase;
+var unittext;
+var vsize;
+var height;
+var width;
+var text_top;
+var text_bottom;
+var ccimage;
+var globalClock;
+var routineTimer;
+function experimentInit() {
+  // Initialize components for Routine "screen_scale"
+  screen_scaleClock = new util.Clock();
+  thisExp=psychoJS.experiment;
+  win=psychoJS.window;
+  event=psychoJS.eventManager;
+  Array.prototype.append = [].push;
+  shuffle = util.shuffle;
+  webbrowser=window;
+  
+  random=Math.random;
+  randint=function(min, maxplusone) {
+      return Math.floor(Math.random() * (maxplusone - min) ) + min;
+  }
+  
+  round = function(num, n=0) {    
+      return +(Math.round(num + ("e+" + n))  + ("e-" + n));
+  }
+  
+  psychoJS.downloadResources([{name: ("Stimuli/bankcard.png"), path:("Stimuli/bankcard.png")},
+  {name: ("Stimuli/greenCheck.png"), path:("Stimuli/greenCheck.png")},
+  {name: ("Stimuli/redWrong.png"), path:("Stimuli/redWrong.png")},
+  {name: ("Stimuli/HofH2.bmp"), path:("Stimuli/HofH2.bmp")},
+  {name: ("Stimuli/HofS2.bmp"), path:("Stimuli/HofS2.bmp")},
+  {name: ("Stimuli/SofH2.bmp"), path:("Stimuli/SofH2.bmp")},
+  {name: ("Stimuli/SofS2.bmp"), path:("Stimuli/SofS2.bmp")},
+  {name: ("Designs/design1_run1.png"), path:("Designs/design1_run1.png")},
+  {name: ("Designs/design2_run1.png"), path:("Designs/design2_run1.png")},
+  {name: ("Designs/design3_run1.png"), path:("Designs/design3_run1.png")},
+  {name: ("Designs/design4_run1.png"), path:("Designs/design4_run1.png")},
+  {name: ("Designs/design1_run2.png"), path:("Designs/design1_run2.png")},
+  {name: ("Designs/design2_run2.png"), path:("Designs/design2_run2.png")},
+  {name: ("Designs/design3_run2.png"), path:("Designs/design3_run2.png")},
+  {name: ("Designs/design4_run2.png"), path:("Designs/design4_run2.png")},
+  {name: ("Designs/design1_run1.csv"), path:("Designs/design1_run1.csv")},
+  {name: ("Designs/design2_run1.csv"), path:("Designs/design2_run1.csv")},
+  {name: ("Designs/design3_run1.csv"), path:("Designs/design3_run1.csv")},
+  {name: ("Designs/design4_run1.csv"), path:("Designs/design4_run1.csv")},
+  {name: ("Designs/design1_run2.csv"), path:("Designs/design1_run2.csv")},
+  {name: ("Designs/design2_run2.csv"), path:("Designs/design2_run2.csv")},
+  {name: ("Designs/design3_run2.csv"), path:("Designs/design3_run2.csv")},
+  {name: ("Designs/design4_run2.csv"), path:("Designs/design4_run2.csv")}]);
+  
+  expInfo['OS'] = window.navigator.platform;
+  expInfo['xResolution'] = screen.width;
+  expInfo['yResolution'] = screen.height;
+  oldt = 0;
+  x_size = 8.56;
+  y_size = 5.398;
+  screen_height = 0;
+  if ((win.units === "norm")) {
+      x_scale = 0.05;
+      y_scale = 0.1;
+      dbase = 0.0001;
+      unittext = " norm units";
+      vsize = 2;
+  } else {
+      if ((win.units === "pix")) {
+          x_scale = 60;
+          y_scale = 40;
+          dbase = 0.1;
+          unittext = " pixels";
+          vsize = win.size[1];
+      } else {
+          x_scale = 0.05;
+          y_scale = 0.05;
+          dbase = 0.0001;
+          unittext = " height units";
+          vsize = 1;
+      }
+  }
+  height = 3.459;
+  width = 3.459;
+  
+  text_top = new visual.TextStim({
+    win: psychoJS.window,
+    name: 'text_top',
+    text: 'Resize this image to match the size of a credit card with arrow keys',
+    font: 'Arial',
+    units: undefined, 
+    pos: [0, 0.3], height: 0.05,  wrapWidth: undefined, ori: 0,
+    color: new util.Color('white'),  opacity: 1,
+    depth: -2.0 
+  });
+  
+  text_bottom = new visual.TextStim({
+    win: psychoJS.window,
+    name: 'text_bottom',
+    text: 'Press Space when you’re finished',
+    font: 'Arial',
+    units: undefined, 
+    pos: [0, (- 0.3)], height: 0.05,  wrapWidth: undefined, ori: 0,
+    color: new util.Color('white'),  opacity: 1,
+    depth: -3.0 
+  });
+  
+  ccimage = new visual.ImageStim({
+    win : psychoJS.window,
+    name : 'ccimage', units : undefined, 
+    image : 'Stimuli/bank-1300155_640.png', mask : undefined,
+    ori : 0, pos : [0, 0], size : [(x_size * x_scale), (y_size * y_scale)],
+    color : new util.Color([1, 1, 1]), opacity : 1,
+    flipHoriz : false, flipVert : false,
+    texRes : 512, interpolate : true, depth : -4.0 
+  });
+  // Create some handy timers
+  globalClock = new util.Clock();  // to track the time since experiment started
+  routineTimer = new util.CountdownTimer();  // to track time remaining of each (non-slip) routine
+  
+  return Scheduler.Event.NEXT;
+}
+
+
+var t;
+var frameN;
+var screen_scaleComponents;
+function screen_scaleRoutineBegin(snapshot) {
+  return function () {
+    //------Prepare to start Routine 'screen_scale'-------
+    t = 0;
+    screen_scaleClock.reset(); // clock
+    frameN = -1;
+    // update component parameters for each repeat
+    // keep track of which components have finished
+    screen_scaleComponents = [];
+    screen_scaleComponents.push(text_top);
+    screen_scaleComponents.push(text_bottom);
+    screen_scaleComponents.push(ccimage);
+    
+    screen_scaleComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent)
+        thisComponent.status = PsychoJS.Status.NOT_STARTED;
+       });
+    
+    return Scheduler.Event.NEXT;
+  };
+}
+
+
+var _pj;
+var keys;
+var dscale;
+var continueRoutine;
+function screen_scaleRoutineEachFrame(snapshot) {
+  return function () {
+    //------Loop for each frame of Routine 'screen_scale'-------
+    let continueRoutine = true; // until we're told otherwise
+    // get current time
+    t = screen_scaleClock.getTime();
+    frameN = frameN + 1;// number of completed frames (so 0 is the first frame)
+    // update/draw components on each frame
+    var _pj;
+    function _pj_snippets(container) {
+        function in_es6(left, right) {
+            if (((right instanceof Array) || ((typeof right) === "string"))) {
+                return (right.indexOf(left) > (- 1));
+            } else {
+                if (((right instanceof Map) || (right instanceof Set) || (right instanceof WeakMap) || (right instanceof WeakSet))) {
+                    return right.has(left);
+                } else {
+                    return (left in right);
+                }
+            }
+        }
+        container["in_es6"] = in_es6;
+        return container;
+    }
+    _pj = {};
+    _pj_snippets(_pj);
+    keys = event.getKeys();
+    if (keys.length) {
+        if (((t - oldt) < 0.5)) {
+            dscale = (5 * dbase);
+            oldt = t;
+        } else {
+            dscale = dbase;
+            oldt = t;
+        }
+        if (_pj.in_es6("space", keys)) {
+            continueRoutine = false;
+        } else {
+            if (_pj.in_es6("up", keys)) {
+                y_scale = (round(((y_scale + dscale) * 10000)) / 10000);
+            } else {
+                if (_pj.in_es6("down", keys)) {
+                    y_scale = (round(((y_scale - dscale) * 10000)) / 10000);
+                } else {
+                    if (_pj.in_es6("left", keys)) {
+                        x_scale = (round(((x_scale - dscale) * 10000)) / 10000);
+                    } else {
+                        if (_pj.in_es6("right", keys)) {
+                            x_scale = (round(((x_scale + dscale) * 10000)) / 10000);
+                        }
+                    }
+                }
+            }
+        }
+        screen_height = (round(((vsize * 10) / y_scale)) / 10);
+        ccimage.size = [(x_size * x_scale), (y_size * y_scale)];
+    }
+    
+    
+    // *text_top* updates
+    if (t >= 0.0 && text_top.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      text_top.tStart = t;  // (not accounting for frame time here)
+      text_top.frameNStart = frameN;  // exact frame index
+      
+      text_top.setAutoDraw(true);
+    }
+
+    
+    // *text_bottom* updates
+    if (t >= 0.0 && text_bottom.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      text_bottom.tStart = t;  // (not accounting for frame time here)
+      text_bottom.frameNStart = frameN;  // exact frame index
+      
+      text_bottom.setAutoDraw(true);
+    }
+
+    
+    // *ccimage* updates
+    if (t >= 0.0 && ccimage.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      ccimage.tStart = t;  // (not accounting for frame time here)
+      ccimage.frameNStart = frameN;  // exact frame index
+      
+      ccimage.setAutoDraw(true);
+    }
+
+    // check for quit (typically the Esc key)
+    if (psychoJS.experiment.experimentEnded || psychoJS.eventManager.getKeys({keyList:['escape']}).length > 0) {
+      return quitPsychoJS('The [Escape] key was pressed. Goodbye!', false);
+    }
+    
+    // check if the Routine should terminate
+    if (!continueRoutine) {  // a component has requested a forced-end of Routine
+      return Scheduler.Event.NEXT;
+    }
+    
+    continueRoutine = false;  // reverts to True if at least one component still running
+    screen_scaleComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent && thisComponent.status !== PsychoJS.Status.FINISHED) {
+        continueRoutine = true;
+      }
+    });
+    
+    // refresh the screen if continuing
+    if (continueRoutine) {
+      return Scheduler.Event.FLIP_REPEAT;
+    } else {
+      return Scheduler.Event.NEXT;
+    }
+  };
+}
+
+
+function screen_scaleRoutineEnd(snapshot) {
+  return function () {
+    //------Ending Routine 'screen_scale'-------
+    screen_scaleComponents.forEach( function(thisComponent) {
+      if (typeof thisComponent.setAutoDraw === 'function') {
+        thisComponent.setAutoDraw(false);
+      }
+    });
+    thisExp.addData("X Scale", x_scale);
+    thisExp.addData("Y Scale", y_scale);
+    
+    // the Routine "screen_scale" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset();
+    
+    return Scheduler.Event.NEXT;
+  };
+}
+
+
+function endLoopIteration(scheduler, snapshot) {
+  // ------Prepare for next entry------
+  return function () {
+    if (typeof snapshot !== 'undefined') {
+      // ------Check if user ended loop early------
+      if (snapshot.finished) {
+        // Check for and save orphaned data
+        if (psychoJS.experiment.isEntryEmpty()) {
+          psychoJS.experiment.nextEntry(snapshot);
+        }
+        scheduler.stop();
+      } else {
+        const thisTrial = snapshot.getCurrentTrial();
+        if (typeof thisTrial === 'undefined' || !('isTrials' in thisTrial) || thisTrial.isTrials) {
+          psychoJS.experiment.nextEntry(snapshot);
+        }
+      }
+    return Scheduler.Event.NEXT;
+    }
+  };
+}
+
+
+function importConditions(currentLoop) {
+  return function () {
+    psychoJS.importAttributes(currentLoop.getCurrentTrial());
+    return Scheduler.Event.NEXT;
+    };
+}
+
+
+function quitPsychoJS(message, isCompleted) {
+  // Check for and save orphaned data
+  if (psychoJS.experiment.isEntryEmpty()) {
+    psychoJS.experiment.nextEntry();
+  }
+  
+  
+  
+  
+  psychoJS.window.close();
+  psychoJS.quit({message: message, isCompleted: isCompleted});
+  
+  return Scheduler.Event.QUIT;
+}
